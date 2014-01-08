2acc5ca360b73dc50866951a13ddaf34cf53abec
Bent Cardan <reqshark@users.noreply.github.com>
2014-01-08 07:58:54 -0500

    still trying to figure out spaces or tabs
    i think that should do it.

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 var spawn = require('child_process').spawn
-	,	fs = require('fs')
+ , fs = require('fs')
 
 
 function stop(n){
-	n.kill('SIGHUP')
+  n.kill('SIGHUP')
 }
 
 function watch(n){
-	fs.watch('views/partials', function (e,f) {
-		if (f && e == 'change') {
-			return stop(n)
-		}
-	})
+  fs.watch('views/partials', function (e,f) {
+    if (f && e == 'change') {
+      return stop(n)
+    }
+  })
 }
 
 function start(){
-	var s = spawn('node', ['app'])
-	
-	s.stdout.on('data', function (data) {
-		var buf = new Buffer(data)
-		console.info(buf.toString())
-	}).on('close', function (code) {
-		return setImmediate(start())
-	})
-	
-	watch(s)
+  var s = spawn('node', ['app'])
+  
+  s.stdout.on('data', function (data) {
+    var buf = new Buffer(data)
+    console.info(buf.toString())
+  }).on('close', function (code) {
+    return setImmediate(start())
+  })
+  
+  watch(s)
 }
 
 start()
