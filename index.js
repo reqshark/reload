3d2caff412df8cf4c8c78a6b8e1ea3056171537b
Bent Cardan <reqshark@users.noreply.github.com>
2014-01-08 07:51:20 -0500

    some reload functions

---
+++ index.js
@@ -0,0 +1,30 @@
+var spawn = require('child_process').spawn
+	,	fs = require('fs')
+
+
+function stop(n){
+	n.kill('SIGHUP')
+}
+
+function watch(n){
+	fs.watch('views/partials', function (e,f) {
+		if (f && e == 'change') {
+			return stop(n)
+		}
+	})
+}
+
+function start(){
+	var s = spawn('node', ['app'])
+	
+	s.stdout.on('data', function (data) {
+		var buf = new Buffer(data)
+		console.info(buf.toString())
+	}).on('close', function (code) {
+		return setImmediate(start())
+	})
+	
+	watch(s)
+}
+
+start()
