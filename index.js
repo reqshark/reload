24c98e12f87cecac934368932bf5818e14142ff3
Bent Cardan <reqshark@users.noreply.github.com>
2014-01-10 22:04:19 -0500

    VIM fix
    mvim's buffer on osx emits several rename events that must be filtered before returning the stop function and restarting the watcher

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-var spawn = require('child_process').spawn
- , fs = require('fs')
 
+var spawn = require('child_process').spawn
+var fs = require('fs')
+var glob = require('glob');
+
+var dir = 'views/partials/';
+var ext = 'jade';
 
 function stop(n){
   n.kill('SIGHUP')
 }
 
 function watch(n){
-  fs.watch('views/partials', function (e,f) {
-    if (f && e == 'change') {
+  fs.watch(dir, function (e,f) {
+    
+    
+    if (e == 'change') {
       return stop(n)
     }
+    
+    
+    //vim compatibility
+    if (e == 'rename') {
+      glob(dir'*.'+ext, function (er, fs){
+        
+        if (er) throw er;
+        
+        for (var i=0; fs[i]; i++)
+          if (f == fs[i].split('.')[1]) {
+            return stop(n)
+          }
+      
+      })
+    }
+    
   })
 }
 
